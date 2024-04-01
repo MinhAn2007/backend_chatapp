a37b2d039df4d2fe246ba81be73fe703886e4098
minhan <voongocminhan20072002@gmail.com>
2024-04-01 11:22:40 +0700

    delete ultils

--- models/OTP.js
+++ models/OTP.js
@@ -1,5 +1,4 @@
 const mongoose= require("mongoose")
-const mailSender = require('../utils/mailSender')
 
 const OTPSchema = new mongoose.Schema({
     email: {
@@ -16,34 +15,6 @@ const OTPSchema = new mongoose.Schema({
 		expires: 60 * 5, // The document will be automatically deleted after 5 minutes of its creation time
 	},
 })
-
-// Define a function to send emails
-async function sendVerificationEmail(email, otp) {
-	// Send the email using our custom mailSender Function
-	try {
-		const mailResponse = await mailSender(
-			email,
-			"Verification Email",
-			`<h1>Please confirm your OTP </h1>
-             <p> here is your OTP code:-> ${otp} </p>
-            `
-		);
-		console.log("Email sent successfully: ", mailResponse);
-	} catch (error) {
-		console.log("Error occurred while sending email: ", error);
-		throw error;
-	}
-}
-
-OTPSchema.pre("save", async function (next) {
-	console.log("New document saved to database");
-
-	// Only send an email when a new document is created
-	if (this.isNew) {
-		await sendVerificationEmail(this.email, this.otp);
-	}
-	next();
-});
 
 const OTP = mongoose.model("OTP", OTPSchema);
 
