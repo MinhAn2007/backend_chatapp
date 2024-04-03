1139c8ae7875b50010776cbedba9a4f51d8d0676
minhan <voongocminhan20072002@gmail.com>
2024-04-03 13:32:42 +0700

    add watch endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 
 const app = express()
+const expressListEndpoints = require('express-list-endpoints');
 
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
@@ -15,6 +16,7 @@ require('./config/database').connect()
 const user = require('./routes/user')
 
 app.use('/user', user)
+console.log(expressListEndpoints(app));
 
 app.listen(PORT, ()=>{
     console.log("Server Started in ", PORT)
