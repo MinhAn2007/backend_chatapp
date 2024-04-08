65e22e3a8014396ca01038393069a6b4f088801c
MinhAn <voongocminhan20072002@gmail.com>
2024-04-08 09:19:53 +0700

    add simple chat

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const router = express.Router()
 //Handlers from controllers
 const { login, signup, sendotp } = require("../controllers/auth")
 const { updateUser, updatePassword, uploadAvatarS3, resetPassword } = require('../controllers/user')
+const { addMessage, getMessages } = require("../controllers/message");
+
 router.post('/login', login)
 router.post('/signup', signup)
 router.post('/sendotp', sendotp)
@@ -11,5 +13,7 @@ router.put('/updatePassword/:email', updatePassword)
 router.put('/updateUser/:userId', updateUser)
 router.post('/uploadAvatarS3/:userId', uploadAvatarS3)
 router.post('/resetPassword',resetPassword)
+router.post("/addmsg/", addMessage);
+router.post("/getmsg/", getMessages);
 
 module.exports = router
